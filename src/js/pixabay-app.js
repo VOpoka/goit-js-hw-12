a7dfab48ea07c94631f825e0669b1a45c35056ab
VOpoka <144518288+VOpoka@users.noreply.github.com>
2024-02-25 19:51:10 +0100

    upload

--- src/js/pixabay-app.js
+++ src/js/pixabay-app.js
@@ -1,56 +1,52 @@
-import axios from "axios";
+import axios from 'axios';
 
 import { value } from '../main.js';
 import { page } from '../main.js';
 
-
 export async function getPhotoBySearch(value, currentPage) {
-    try {
-        const KEY = '42424645-ecd3f1048329df1dec069e6a8';
-        const response = await axios.get('https://pixabay.com/api/', {
-            params: {
-                key: KEY,
-                q: value,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: 'true',
-                page: currentPage,
-                per_page: '15'
-            }
-        });
+  try {
+    const KEY = '42516413-5d4d39fc32c8318bac1e6503b';
+    const response = await axios.get('https://pixabay.com/app/', {
+      params: {
+        key: KEY,
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: currentPage,
+        per_page: '15',
+      },
+    });
 
-        if (response.data.total === 0) {
-            throw new Error('No images found');
-        }
-
-      return response.data;
-    } catch (error) {
-        throw error; 
+    if (response.data.total === 0) {
+      throw new Error('Found no images');
     }
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-
 export async function fetchMoreImages(value, currentPage) {
-    try {
-        const params = new URLSearchParams({
-            key: '42424645-ecd3f1048329df1dec069e6a8',
-            q: value,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: 'true',
-            per_page: '15',
-            page: currentPage
-        });
+  try {
+    const params = new URLSearchParams({
+      key: '42516413-5d4d39fc32c8318bac1e6503b',
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: '15',
+      page: currentPage,
+    });
 
-      let images = await axios.get(
-            `https://pixabay.com/api/?${params}`
-        );
-        if (images.data.total === 0) {
-            throw new Error('No images found');
-        }
-
-        return images.data;
-    } catch (error) {
-        throw error;
+    let images = await axios.get(`https://pixabay.com/app/?${params}`);
+    if (images.data.total === 0) {
+      throw new Error('Found no images');
     }
+
+    return images.data;
+  } catch (error) {
+    throw error;
+  }
 }
