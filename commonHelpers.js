910d6489f1f45f2d4d777f8b725a71cee1a0f6aa
VOpoka <144518288+VOpoka@users.noreply.github.com>
2024-02-27 11:28:57 +0000

    Deploying to gh-pages from @ VOpoka/goit-js-hw-12@94928a074e8acc3bca4404fe83b3cd310bd8b219 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{S as f,a as B,i as w}from"./assets/vendor-5401a4b0.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const m=document.querySelector(".gallery-o"),b=document.querySelector(".search-form");new f(".gallery-o");const v={captions:!0,captionSelector:"img",captionType:"attr",captionsData:"alt",captionPosition:"bottom",animation:250};function T(s){const n=s.map(({largeImageURL:r,webformatURL:e,tags:t,likes:i,views:l,comments:d,downloads:u})=>`
+import{S as O,a as P,i as g}from"./assets/vendor-5401a4b0.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const u=document.querySelector(".gallery-o"),L=document.querySelector(".search-form"),B={captions:!0,captionSelector:"img",captionType:"attr",captionsData:"alt",captionPosition:"bottom",animation:250},v=new O(".gallery-o a",B);function T(o){const n=o.map(({largeImageURL:s,webformatURL:r,tags:e,likes:t,views:i,comments:l,downloads:d})=>`
         <div class="gallery">
-            <a href="${r}">
-            <img src="${e}" alt="${t}" title="${t}" width="380" height="220" />
+            <a href="${s}">
+            <img src="${r}" alt="${e}" title="${e}" width="380" height="220" />
             <ul class="info-cards-container">
-                <li class="info-cards-elements">likes<span>${i}</span></li>
-                <li class="info-cards-elements">views<span>${l}</span></li>
-                <li class="info-cards-elements">comments<span>${d}</span></li>
-                <li class="info-cards-elements">downloads<span>${u}</span></li>
+                <li class="info-cards-elements">likes<span>${t}</span></li>
+                <li class="info-cards-elements">views<span>${i}</span></li>
+                <li class="info-cards-elements">comments<span>${l}</span></li>
+                <li class="info-cards-elements">downloads<span>${d}</span></li>
             </ul>
             </a>
         </div>
-        `).join("");m.innerHTML=n;const o=new f(".gallery-o a",v);o.on("show.simplelightbox"),o.refresh(),b.reset()}function H(s){const o=s.hits.map(({largeImageURL:t,webformatURL:i,tags:l,likes:d,views:u,comments:O,downloads:P})=>`
+        `).join("");u.innerHTML=n,v.refresh(),L.reset()}function H(o){const s=o.hits.map(({largeImageURL:e,webformatURL:t,tags:i,likes:l,views:d,comments:q,downloads:x})=>`
         <div class="gallery">
-            <a href="${t}">
-            <img src="${i}" alt="${l}" title="${l}" width="380" height="220" />
+            <a href="${e}">
+            <img src="${t}" alt="${i}" title="${i}" width="380" height="220" />
             <ul class="info-cards-container">
-                <li class="info-cards-elements">likes<span>${d}</span></li>
-                <li class="info-cards-elements">views<span>${u}</span></li>
-                <li class="info-cards-elements">comments<span>${O}</span></li>
-                <li class="info-cards-elements">downloads<span>${P}</span></li>
+                <li class="info-cards-elements">likes<span>${l}</span></li>
+                <li class="info-cards-elements">views<span>${d}</span></li>
+                <li class="info-cards-elements">comments<span>${q}</span></li>
+                <li class="info-cards-elements">downloads<span>${x}</span></li>
             </ul>
             </a>
         </div>
-        `).join("");m.insertAdjacentHTML("beforeend",o);const r=new f(".gallery-o a",v);r.on("show.simplelightbox"),r.refresh(),b.reset();const e=m.firstElementChild.getBoundingClientRect().height;scrollBy({behavior:"smooth",top:e*2})}async function $(s,n){try{const o="42516413-5d4d39fc32c8318bac1e6503b",r=await B.get("https://pixabay.com/api/",{params:{key:o,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true",page:n,per_page:"15"}});if(r.data.total===0)throw new Error("No images found");return r.data}catch(o){throw o}}const g=document.querySelector(".search-form"),S=document.querySelector(".gallery-o"),E=document.querySelector(".loader"),x=document.querySelector(".loader2"),c=document.querySelector(".more-btn");let h,a,p;document.addEventListener("DOMContentLoaded",()=>{L(),g.addEventListener("submit",s);async function s(n){n.preventDefault(),A(),q(),S.innerHTML="",a=1,h=g.querySelector(".input-search").value;try{const o=await $(h,a);T(o.hits),p=Math.ceil(o.totalHits/15),y()}catch(o){k(o)}finally{L()}}});function k(s){S.innerHTML="",w.show({message:`❌ "${s}" Please try again!`,color:"red",position:"topRight",maxWidth:"400px"})}function A(){E.style.display="block"}function L(){E.style.display="none"}function C(){x.classList.remove("hidden")}function M(){x.classList.add("hidden")}c.addEventListener("click",async()=>{a+=1,C();try{const s=await $(h,a);H(s),a>1&&(M(),y())}catch(s){console.log(s)}});function N(){a===p&&(M(),y(),w.show({message:'❌ "Sorry, you have reached the end of your search results."',color:"red",position:"bottomRight",maxWidth:"320px"}))}c.addEventListener("click",N);function j(){c.classList.remove("hidden")}function q(){c.classList.add("hidden")}function y(){a>=p?q():j()}
+        `).join("");u.insertAdjacentHTML("beforeend",s),v.refresh(),L.reset();const r=u.firstElementChild.getBoundingClientRect().height;scrollBy({behavior:"smooth",top:r*2})}async function w(o,n){try{const s="42516413-5d4d39fc32c8318bac1e6503b",r=await P.get("https://pixabay.com/api/",{params:{key:s,q:o,image_type:"photo",orientation:"horizontal",safesearch:"true",page:n,per_page:"15"}});if(r.data.total===0)throw new Error("No images found");return r.data}catch(s){throw s}}const p=document.querySelector(".search-form"),$=document.querySelector(".gallery-o"),b=document.querySelector(".loader"),S=document.querySelector(".loader2"),c=document.querySelector(".more-btn");let m,a,f;document.addEventListener("DOMContentLoaded",()=>{y(),p.addEventListener("submit",o);async function o(n){n.preventDefault(),A(),M(),$.innerHTML="",a=1,m=p.querySelector(".input-search").value;try{const s=await w(m,a);T(s.hits),f=Math.ceil(s.totalHits/15),h()}catch(s){k(s)}finally{y()}}});function k(o){$.innerHTML="",g.show({message:`❌ "${o}" Please try again!`,color:"red",position:"topRight",maxWidth:"400px"})}function A(){b.style.display="block"}function y(){b.style.display="none"}function C(){S.classList.remove("hidden")}function E(){S.classList.add("hidden")}c.addEventListener("click",async()=>{a+=1,C();try{const o=await w(m,a);H(o),a>1&&(E(),h())}catch(o){console.log(o)}});function N(){a===f&&(E(),h(),g.show({message:'❌ "Sorry, you have reached the end of your search results."',color:"red",position:"bottomRight",maxWidth:"320px"}))}c.addEventListener("click",N);function j(){c.classList.remove("hidden")}function M(){c.classList.add("hidden")}function h(){a>=f?M():j()}
 //# sourceMappingURL=commonHelpers.js.map
